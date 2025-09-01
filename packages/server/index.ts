c49b70d1527acb9e0d13335d6a50217cf3560a53
myro-aiden <mdiane3620@gmail.com>
2025-09-01 12:53:16 -0400

    Setup Conversation Repository

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
+import z from 'zod';
+import { conversationRepository } from './repositries/conversation.repositry';
 
 dotenv.config();
 
@@ -19,16 +21,51 @@ app.get('/api/hello', (req: Request, res: Response) => {
    res.json({ message: 'Hello from the server! yer' });
 });
 
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'Prompt is required.')
+      .max(1000, 'Prompt is too long. Max 1000 characters.'),
+   conversationID: z.uuid(),
+});
+
 app.post('/api/chat', async (req: Request, res: Response) => {
-   const { prompt } = req.body;
+   try {
+      const { prompt, conversationID } = req.body;
+      const response = await client.responses.create({
+         model: 'gpt-4.1-nano',
+         input: prompt,
+         temperature: 0.2,
+         max_output_tokens: 50,
+         previous_response_id:
+            conversationRepository.getLastResponseId(conversationID),
+      });
+
+      conversationRepository.setLastResponseId(conversationID, response.id);
+
+      res.json({ message: response.output_text });
+   } catch (error) {
+      res.status(500).json({
+         error: 'Internal Server Error, failed to generate response',
+      });
+   }
+
+   const parseResult = chatSchema.safeParse(req.body);
+   if (!parseResult.success) {
+      res.status(400).json(parseResult.error?.format());
+      return;
+   }
 
    const response = await client.responses.create({
       model: 'gpt-4.1-nano',
       input: prompt,
       temperature: 0.2,
       max_output_tokens: 50,
+      previous_response_id: conversations.get(conversationID),
    });
 
+   conversations.set(conversationID, response.id);
    res.json({ message: response.output_text });
 });
 
---
+++ packages/server/repositries/conversation.repositry.ts
@@ -0,0 +1,12 @@
+// implementaion of in-memory conversation repository / implementation detail / keep privsate
+const conversations = new Map<string, string>();
+
+export const conversationRepository = {
+   getLastResponseId(conversationId: string) {
+      return conversations.get(conversationId);
+   },
+
+   setLastResponseId(conversationId: string, responseId: string) {
+      conversations.set(conversationId, responseId);
+   },
+};
